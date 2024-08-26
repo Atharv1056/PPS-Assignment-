215140acca1e3584676567c0a0f27146879002a5
Atharv Ambre <atharv.ambre24@sanjivani.edu.in>
2024-08-26 11:12:29 +0530

    Update PPS Assignment 1.cpp

--- PPS Assignment 1.cpp
+++ PPS Assignment 1.cpp
@@ -13,15 +13,15 @@ class Car
 int main()
 {
   Car carobj1;
-  carobj1.brand="Dodge";
-  carobj1.model=" Challenger SRT ";
-  carobj1.year= 1982;
+  carobj1.brand="BMW";
+  carobj1.model=" BMW M5 ";
+  carobj1.year= 1998;
   
   //creating another object of car 
   Car carobj2;
-  carobj2.brand="Nissan";
-  carobj2.model=" Skyline GT-r";
-  carobj2.year= 1972;
+  carobj2.brand="Lamborghini";
+  carobj2.model="Huracan";
+  carobj2.year= 2013;
   
   cout<<carobj1.brand<<carobj1.model<<carobj1.year<<endl;
   cout<<carobj2.brand<<carobj2.model<<carobj2.year<<endl;
